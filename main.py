ac8e3095af6f0f7cfec27cb91a01ae0f1110f386
Dongli99 <dongliliu0@gmail.com>
2024-03-18 12:11:39 -0700

    draw ticks on the gauge

--- main.py
+++ main.py
@@ -27,17 +27,26 @@ class Gauge(Canvas):
         self.drawGauge()
 
     def drawGauge(self):
+        TICK_LEN = 10
+        TICK_WID = 3
+        ARC_WIDTH = 3
+        self.drawTick(Gauge.MIN_VALUE, TICK_LEN, TICK_WID, ARC_WIDTH / 2)
         for i in range(Gauge.PARTITION):
             self.drawArc(
                 Gauge.START_LOC,
                 Gauge.SIZE,
                 Gauge.MAX_ANGLE / Gauge.PARTITION * i - (Gauge.MAX_ANGLE - 180) / 2,
                 Gauge.MAX_ANGLE / Gauge.PARTITION,
-                self.generate_colors(Gauge.COLORS_RANGE, Gauge.PARTITION - 1)[i],
+                self.generateColors(Gauge.COLORS_RANGE, Gauge.PARTITION - 1)[i],
+                ARC_WIDTH,
             )
+            self.drawTick(
+                Gauge.MAX_VALUE / Gauge.PARTITION * i, TICK_LEN, TICK_WID, ARC_WIDTH / 2
+            )
+        self.drawTick(Gauge.MAX_VALUE, TICK_LEN, TICK_WID, ARC_WIDTH / 2)
         self.drawPointer(180)
 
-    def drawArc(self, start_loc, size, start_angle, angle, color):
+    def drawArc(self, start_loc, size, start_angle, angle, color, wid):
         self.create_arc(
             start_loc["x"],
             start_loc["y"],  # top left
@@ -47,56 +56,73 @@ class Gauge(Canvas):
             extent=angle,  # start angle how far to go
             style="arc",  # draw arc rather than fan
             outline=color,
-            width=5,
+            width=wid,
         )
 
-    def drawTick(self, size, angle):
-        pass
-
-    def drawPointer(self, value):
-        def get_loc_on_arc(center_x, center_y, r, theta):
-            theta = radians(theta)
-            x = r * cos(theta) + center_x
-            y = center_y - r * sin(theta)  # Invert y due to Tkinter's coordinate system
-            return x, y
-
-        PIVOT_SIZE = 5
-        PERCENT_UP = 0.8
+    def drawTick(self, value, len, wid, offset):
         x_center = Gauge.START_LOC["x"] + Gauge.SIZE / 2
         y_center = Gauge.START_LOC["y"] + Gauge.SIZE / 2
-        # draw the pivot
-        self.create_oval(
-            x_center - PIVOT_SIZE,
-            y_center - PIVOT_SIZE,
-            x_center + PIVOT_SIZE,
-            y_center + PIVOT_SIZE,
-            fill="silver",
-            outline="",
+        theta = 180 - (
+            value * Gauge.MAX_ANGLE / (Gauge.MAX_VALUE - Gauge.MIN_VALUE)
+            - (Gauge.MAX_ANGLE - 180) / 2
         )
+        up_x, up_y = self.getLocOnArc(
+            x_center, y_center, Gauge.SIZE / 2 + offset, theta
+        )
+        down_x, down_y = self.getLocOnArc(
+            x_center, y_center, Gauge.SIZE / 2 + offset - len, theta
+        )
+        self.create_line(up_x, up_y, down_x, down_y, width=wid)
+
+    def drawPointer(self, value):
+        PIVOT_SIZE = 5
+        PIVOT_COL = "silver"
+        POINTER_UP = 0.8
+        x_center = Gauge.START_LOC["x"] + Gauge.SIZE / 2
+        y_center = Gauge.START_LOC["y"] + Gauge.SIZE / 2
+        self.drawPivot(x_center, y_center, PIVOT_SIZE, PIVOT_COL)
         # draw the pointer
         theta = 180 - (
-            value * Gauge.MAX_ANGLE / Gauge.MAX_VALUE - (Gauge.MAX_ANGLE - 180) / 2
+            value * Gauge.MAX_ANGLE / (Gauge.MAX_VALUE - Gauge.MIN_VALUE)
+            - (Gauge.MAX_ANGLE - 180) / 2
         )
-        up_x, up_y = get_loc_on_arc(
-            x_center, y_center, Gauge.SIZE * PERCENT_UP / 2, theta
+        up_x, up_y = self.getLocOnArc(
+            x_center, y_center, Gauge.SIZE * POINTER_UP / 2, theta
         )
-        down_x, down_y = get_loc_on_arc(
-            x_center, y_center, Gauge.SIZE * (1 - PERCENT_UP) / 2, theta + 180
+        down_x, down_y = self.getLocOnArc(
+            x_center, y_center, Gauge.SIZE * (1 - POINTER_UP) / 2, theta + 180
         )
-        self.create_line(up_x, up_y, down_x, down_y)
+        self.create_line(up_x, up_y, down_x, down_y, width=3)
 
-    def set_value(self, value):
+    def drawPivot(self, x_center, y_center, size, color):
+        # draw the pivot
+        self.create_oval(
+            x_center - size,
+            y_center - size,
+            x_center + size,
+            y_center + size,
+            fill=color,
+            outline="",
+        )
+
+    def setValue(self, value):
         pass
 
-    def generate_colors(self, c_range, steps):
-        def hex_color(rgb):
+    def getLocOnArc(self, center_x, center_y, r, theta):
+        theta = radians(theta)
+        x = r * cos(theta) + center_x
+        y = center_y - r * sin(theta)  # Invert y due to Tkinter's coordinate system
+        return x, y
+
+    def generateColors(self, c_range, steps):
+        def hexColor(rgb):
             red = int(rgb[0])
             green = int(rgb[1])
             blue = int(rgb[2])
             return "#{:02x}{:02x}{:02x}".format(red, green, blue)
 
         colors = []
-        colors.append(hex_color(c_range[0]))
+        colors.append(hexColor(c_range[0]))
         red = c_range[0][0]
         green = c_range[0][1]
         blue = c_range[0][2]
@@ -104,8 +130,8 @@ class Gauge(Canvas):
             red += (c_range[-1][0] - c_range[0][0]) / steps
             green += (c_range[-1][1] - c_range[0][1]) / steps
             blue += (c_range[-1][2] - c_range[0][2]) / steps
-            colors.append(hex_color([red, green, blue]))
-        colors.append(hex_color(c_range[-1]))
+            colors.append(hexColor([red, green, blue]))
+        colors.append(hexColor(c_range[-1]))
         return colors
 
 
