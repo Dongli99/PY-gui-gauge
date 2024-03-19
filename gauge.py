9eeb63dac56849f3297bff30d7609a624573d181
Dongli99 <dongliliu0@gmail.com>
2024-03-19 08:32:15 -0700

    gauge reaches its milestone

--- gauge.py
+++ gauge.py
@@ -2,10 +2,22 @@ import random
 from tkinter import *
 from tkinter import messagebox
 from math import sin, cos, radians
-from util import generateColors, generateRandomColor
 
 
 class Gauge(Frame):
+    """
+    Gauge class for creating a customizable gauge widget.
+
+    Attributes:
+        value (int): The current value of the gauge.
+        max_v (int): The maximum value of the gauge.
+        min_v (int): The minimum value of the gauge.
+        max_angle (int): The maximum angle (in degrees) of the gauge.
+        size (int): The size of the gauge.
+        partition (int): The number of partitions in the gauge.
+        loc (tuple): The position of the gauge.
+        col_range (list): The range of colors for the gauge.
+    """
 
     def __init__(
         self,
@@ -18,9 +30,21 @@ class Gauge(Frame):
         loc=(50, 80),
         col_range=[[255, 215, 0], [32, 178, 170]],
     ):
+        """
+        Initialize the Gauge instance with default values or specified parameters.
+
+        Args:
+            value (int): The current value of the gauge (default is 0).
+            max_v (int): The maximum value of the gauge (default is 250).
+            min_v (int): The minimum value of the gauge (default is 0).
+            max_angle (int): The maximum angle (in degrees) of the gauge (default is 240).
+            size (int): The size of the gauge (default is 320).
+            partition (int): The number of partitions in the gauge (default is 4).
+            loc (tuple): The position of the gauge (default is (50, 80)).
+            col_range (list): The range of colors for the gauge (default is [[255, 215, 0], [32, 178, 170]]).
+        """
         super().__init__()
         self.value = value
-        self.value_var = IntVar()
         self.max_v = max_v
         self.min_v = min_v
         self.max_angle = max_angle
@@ -28,22 +52,43 @@ class Gauge(Frame):
         self.partition = partition
         self.loc = loc
         self.col_range = col_range
+        self.tick_wid = 3
+        self.arc_wid = 10
+        self.value_var = IntVar()  # Variable to store value entered by user
         self.initUI()
 
+    def initUI(self):
+        """Initialize the graphical user interface (GUI) of the gauge."""
+        self.master.title("Lab 9 - Gauge")
+        self.pack(fill=BOTH, expand=1)
+        self.drawTop()  # Draw the top part of the gauge
+        self.drawBottom()  # Draw the bottom part of the gauge
+
     def setValue(self, value):
+        """
+        Set the value of the gauge and update the display.
+
+        Args:
+            value (int): The new value of the gauge.
+        """
         self.value = value
         self.canvas.delete("pointer")
         self.canvas.delete("display_box")
         self.drawPointer(self.value)
         self.drawDisplay(self.value)
 
-    def initUI(self):
-        self.master.title("Lab 9 A & B")
-        self.pack(fill=BOTH, expand=1)
-        self.drawTop()
-        self.drawBottom()
-
     def drawArc(self, loc, size, start_angle, angle, color, wid):
+        """
+        Draw an arc on the canvas.
+
+        Args:
+            loc (tuple): Top-left corner coordinates of the bounding box of the arc.
+            size (int): Size of the bounding box (width and height) of the arc.
+            start_angle (int): Starting angle of the arc (in degrees).
+            angle (int): Angle to sweep (in degrees).
+            color (str): Color of the arc.
+            wid (int): Width of the arc.
+        """
         self.canvas.create_arc(
             loc[0],
             loc[1],  # top left
@@ -58,6 +103,15 @@ class Gauge(Frame):
         )
 
     def drawTick(self, value, len, wid, offset):
+        """
+        Draw a tick mark on the gauge.
+
+        Args:
+            value (int): Value corresponding to the tick mark.
+            len (int): Length of the tick mark.
+            wid (int): Width of the tick mark.
+            offset (int): Offset of the tick mark from the center of the gauge.
+        """
         x_center = self.loc[0] + self.size / 2
         y_center = self.loc[1] + self.size / 2
         theta = 180 - (
@@ -68,7 +122,7 @@ class Gauge(Frame):
         down_x, down_y = self.getLocOnArc(
             x_center, y_center, self.size / 2 + offset - len, theta
         )
-        self.canvas.create_line(up_x, up_y, down_x, down_y, width=wid)
+        self.canvas.create_line(up_x, up_y, down_x, down_y, width=wid, fill="#2b2d42")
         tag_x, tag_y = self.getLocOnArc(
             x_center, y_center, self.size / 2 + offset - len - 15, theta
         )
@@ -77,6 +131,12 @@ class Gauge(Frame):
         )
 
     def drawPointer(self, value):
+        """
+        Draw the pointer of the gauge.
+
+        Args:
+            value (int): Value corresponding to the pointer position.
+        """
         PIVOT_SIZE = 5
         PIVOT_COL = "silver"
         POINTER_UP = 0.8
@@ -97,7 +157,15 @@ class Gauge(Frame):
         self.drawPivot(x_center, y_center, PIVOT_SIZE, PIVOT_COL)
 
     def drawPivot(self, x, y, size, color):
-        # draw the pivot
+        """
+        Draw the pivot of the gauge.
+
+        Args:
+            x (int): x-coordinate of the pivot.
+            y (int): y-coordinate of the pivot.
+            size (int): Size of the pivot.
+            color (str): Color of the pivot.
+        """
         self.canvas.create_oval(
             x - size,
             y - size,
@@ -108,6 +176,12 @@ class Gauge(Frame):
         )
 
     def drawDisplay(self, value):
+        """
+        Draw the display of the gauge.
+
+        Args:
+            value (int): Value to be displayed.
+        """
         section = (
             self.partition
             - 1
@@ -128,12 +202,11 @@ class Gauge(Frame):
         self.canvas.tag_raise(text, box)
 
     def drawTop(self):
+        """Draw the top part of the gauge."""
         frame = Frame(self)
         frame.place(relx=0.03, rely=0.03, relwidth=0.94, relheight=0.75)
         self.canvas = Canvas(frame)
         self.canvas.pack(fill=BOTH, expand=1)
-        self.tick_wid = 3
-        self.arc_wid = 10
         self.color_set = generateColors(self.col_range, self.partition - 1)
         self.canvas.create_text(
             self.loc[0] + self.size / 2,
@@ -141,7 +214,6 @@ class Gauge(Frame):
             text="GAUGE",
             font=("Helvetica", 12, "bold"),
         )
-        self.drawTick(self.min_v, self.arc_wid * 2, self.tick_wid, self.arc_wid / 2)
         for i in range(self.partition):
             self.drawArc(
                 self.loc,
@@ -151,17 +223,21 @@ class Gauge(Frame):
                 self.color_set[i],
                 self.arc_wid,
             )
+        for i in range(self.partition):
             self.drawTick(
                 self.max_v / self.partition * i,
                 self.arc_wid * 2,
                 self.tick_wid,
                 self.arc_wid / 2,
             )
+        self.drawTick(self.min_v, self.arc_wid * 2, self.tick_wid, self.arc_wid / 2)
         self.drawTick(self.max_v, self.arc_wid * 2, self.tick_wid, self.arc_wid / 2)
         self.drawPointer(self.value)
         self.drawDisplay(self.value)
 
     def drawBottom(self):
+        """Draw the bottom part of the gauge."""
+
         def handleUpdate():
             value = int(self.value_var.get())
             if value <= self.max_v and value >= self.min_v:
@@ -178,6 +254,7 @@ class Gauge(Frame):
             self.color_set = generateColors(self.col_range, self.partition - 1)
             self.max_angle = random.randint(90, 330)
             self.partition = random.randint(3, 10)
+            self.arc_wid = random.randint(3, 30)
             self.canvas.delete("arc")
             self.drawTop()
             self.setValue(random.randint(0, 250))
@@ -192,10 +269,71 @@ class Gauge(Frame):
         lucky_btn.place(relx=0.32, rely=0.35, relwidth=0.12, relheight=0.4)
 
     def getLocOnArc(self, center_x, center_y, r, theta):
+        """
+        Get the coordinates of a point on an arc.
+
+        Args:
+            center_x (int): x-coordinate of the center of the arc.
+            center_y (int): y-coordinate of the center of the arc.
+            r (int): Radius of the arc.
+            theta (int): Angle (in degrees) of the point on the arc.
+
+        Returns:
+            tuple: The (x, y) coordinates of the point on the arc.
+        """
         theta = radians(theta)
         x = r * cos(theta) + center_x
         y = center_y - r * sin(theta)  # Invert y due to Tkinter's coordinate system
         return x, y
+
+
+def hexColor(rgb):
+    """
+    Convert an RGB color to hexadecimal format.
+
+    Args:
+        rgb (list): List containing the RGB values.
+
+    Returns:
+        str: Hexadecimal representation of the RGB color.
+    """
+    red = int(rgb[0])
+    green = int(rgb[1])
+    blue = int(rgb[2])
+    return "#{:02x}{:02x}{:02x}".format(red, green, blue)
+
+
+def generateColors(c_range, steps):
+    """
+    Generate a list of colors between two given RGB colors.
+
+    Args:
+        c_range (list): List containing the start and end RGB colors.
+        steps (int): Number of steps to generate between the start and end colors.
+
+    Returns:
+        list: List of hexadecimal color codes.
+    """
+    colors = []
+    colors.append(hexColor(c_range[0]))
+    red = c_range[0][0]
+    green = c_range[0][1]
+    blue = c_range[0][2]
+    for i in range(steps):
+        red += (c_range[-1][0] - c_range[0][0]) / steps
+        green += (c_range[-1][1] - c_range[0][1]) / steps
+        blue += (c_range[-1][2] - c_range[0][2]) / steps
+        colors.append(hexColor([red, green, blue]))
+    colors.append(hexColor(c_range[-1]))
+    return colors
+
+
+def generateRandomColor():
+    """Generate a random RGB color."""
+    red = random.randint(0, 255)
+    green = random.randint(0, 255)
+    blue = random.randint(0, 255)
+    return [red, green, blue]
 
 
 if __name__ == "__main__":
